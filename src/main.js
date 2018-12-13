1a5a60d6a1ce456984c917592fbf1ae589f3f7bc
Filipe Dev <italo@infomix.local>
2018-12-13 16:25:25 -0200

    primeiro commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "jquery/dist/jquery";
+import "events/events";
+
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import VueSession from "vue-session";
 import VueResource from "vue-resource";
+
+window.$ = window.jQuery = require("jquery");
 
 Vue.use(VueSession);
 Vue.use(VueResource);
